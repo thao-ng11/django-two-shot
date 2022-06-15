5f1be4d7515d6de93acc3c4be9cf272eeabb48c7
Thao <thao.ng11@gmail.com>
2022-06-15 19:29:26 -0400

    Feature 9 complete

--- expenses/settings.py
+++ expenses/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
