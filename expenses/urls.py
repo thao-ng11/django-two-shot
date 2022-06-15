bc8b5c7d28d714fd3fb841eceae694ca832168e3
Thao <thao.ng11@gmail.com>
2022-06-15 17:31:25 -0400

    Feature 6 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "receipts/",
+        include("receipts.urls"),
+    ),
+    path("", RedirectView.as_view(url=reverse_lazy("home"))),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from receipts.models import Account, ExpenseCategory, Receipt
 
 # Create your views here.
+
+# class ExpenseCategoryListView(ListView):
+#     model = ExpenseCategory
+#     template_name = "receipts/list.html"
+#     paginate_by = 2
+
+
+class ReceiptListView(ListView):
+    model = Receipt
+    template_name = "receipts/list.html"
+    paginate_by = 2
+
+
+# class AccountListView(ListView):
+#     model = Account
+#     template_name = "receipts/list.html"
+#     paginate_by = 2
